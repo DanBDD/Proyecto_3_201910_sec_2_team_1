f5b2fbc64be327a252eac7013b8df373db11eddf
Daniel Betancurth <danielbetancurth@MacBook-Pro-de-Daniel.local>
2019-05-04 15:52:52 -0500

    Falta comprobacion de la carga

---
+++ src/Tests/TestLinearProbing.java
@@ -0,0 +1,56 @@
+package Tests;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+import model.data_structures.LinearProbing;
+
+public class TestLinearProbing extends TestCase{
+
+	protected LinearProbing<String, Integer> linear;
+	@Before
+	public void setUp()
+	{
+		linear=new LinearProbing<>(101);
+		System.out.println("TestLinearProbing");
+		int contador=0;
+		while(linear.keys().hasNext())
+		{
+			contador++;
+		}
+		assertEquals(contador, 0);
+		System.out.println("Tama√±o del arreglo inicial: "+contador);
+		int inic=linear.m();
+		int cambios=0;
+		for(int i=0;i<10000;i++)
+		{
+			linear.put(Integer.toString(i), i);
+			if(linear.m()!=inic)
+			{
+				inic=linear.m();
+				cambios++;
+			}
+		}
+		System.out.println("Cantidad de rehashes: "+cambios);
+	}
+	@Test
+	public void testSize()
+	{
+		//		Tama√±o final arreglo
+		
+		System.out.println("TamaÒo final del arreglo: "+linear.size());
+		System.out.println("Numero de duplas: "+linear.size());
+		System.out.println("Factor de carga final: "+(double)linear.size()/linear.m());
+		long startTime = System.currentTimeMillis();
+		for(int i=0;i<10000;i++)
+		{
+			int v=linear.get(Integer.toString(i));
+		}
+		long endTime = System.currentTimeMillis();
+		long duration = endTime - startTime;
+		double l=(double)duration/10000;
+		System.out.println("Tiempo promedio: "+l+" milisegundos");
+	}
+
+}
---
+++ src/Tests/TestMax.java
@@ -0,0 +1,49 @@
+package Tests;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+import model.data_structures.*;
+
+public class TestMax extends TestCase{
+	protected static final int[] ARREGLO = {350, 383, 105, 233, 140, 266, 356, 236, 80, 360, 221, 241, 130, 244, 352, 446, 18, 98, 97, 396};
+
+	protected MaxColaPrioridad<Integer> cola;
+	protected MaxHeapCP<Integer> heap;
+	@Before
+	public void setUp()
+	{
+		cola= new MaxColaPrioridad<Integer>();
+		heap=new MaxHeapCP<Integer>();
+		for(int actual: ARREGLO)
+		{
+			cola.agregar(actual);
+			heap.agregar(actual);
+		}
+
+	}
+	@Test
+	public void testSize()
+	{
+		assertEquals(cola.darNumElementos(), 20);
+		assertEquals(heap.darNumElementos(), 20);
+
+	}
+	@Test
+	public void testDarMax(){
+		assertEquals(heap.max(), Integer.valueOf(446));
+		assertEquals(cola.max(), Integer.valueOf(446));
+		
+	}
+	@Test
+	public void testDelMax(){
+		assertEquals(cola.delMax(), Integer.valueOf(446));
+		assertEquals(cola.darNumElementos(), 19);
+		assertEquals(heap.delMax(), Integer.valueOf(446));
+		assertEquals(heap.darNumElementos(), 19);
+		
+		
+	}
+	
+}
+
